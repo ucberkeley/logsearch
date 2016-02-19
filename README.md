2e3fa9b6ad9934778037c0bd41c7519516364909
Steve Chan <sychan@berkeley.edu>
2016-02-19 13:56:57 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,152 @@
+## Synopsis
+
+Logsearch - query an elasticsearch server for logs produced by logstash-logback module <https://github.com/logstash/logstash-logback-encoder/tree/logstash-logback-encoder-4.6>
+
+Output from query will be in JSON, and it is recommended that the __jq__ tool be used for formatting and extracting information.
+
+Several commandline flags are supported to query specific fields, but arbitrary search terms can be passed to the elasticsearch interface on the commandline. Note elasticsearch query syntax must be followed (specifically AND and OR must be capitalized)
+
+## Usage Example
+
+Logsearch looks for the startup configuration in ~/.logsearch_profile, formatted in TOML ( <https://npf.io/2014/08/intro-to-toml/> ). Any field that is in the Config struct can be preset in the startup profile.
+
+Here is an example file that shows setting the username and password for authenticating to the elasticsearch endpoint:
+
+    $ cat ~/.logsearch_profile
+    Username="sychan"
+    Password="steves awesome password"
+    
+
+__Get usage info__
+
+    logsearch -h
+    Usage of logsearch:
+      -config string
+            Location of TOML formatted configuration file https://github.com/toml-lang/toml
+            NOTE: setting non-default configfile override comflags
+            (default "~/.logsearch_profile")
+      -context string
+            camel.contextId to match
+      -correlation string
+    	    camel.correlationID to match
+      -count
+            Return only a count of the number of matches
+      -debug
+            Output debug information
+      -errors
+            Return only error logs that match
+      -from string
+            Time specification for starting time of the search
+            https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-date-format.html
+            (default "now-30m")
+      -message string
+            Match against the main log message
+      -numlogs int
+            Number of lines of matching logs to return at a time (default 100)
+      -offset int
+            Offset into total matching logs to start
+      -stack string
+            stack_trace to match
+      -uat
+            Search among the UAT logs for matches
+      -until string
+            Time specification for ending time of search (default "now")
+      -username string
+            Enable basic auth by setting username (default "analytics")
+      -password string
+            Password for basic auth
+
+__Get a count of the number of prod ESB errors in the last 30 minutes (count is in the "count" attribute)__
+
+    logsearch -errors -count | jq .
+    {
+      "_shards": {
+        "failed": 0,
+        "successful": 261,
+        "total": 261
+      },
+      "count": 1
+    }
+ 
+__Error count for the last 6 hours__
+
+    logsearch -errors -count -from now-6h | jq .
+    {
+      "_shards": {
+        "failed": 0,
+        "successful": 261,
+        "total": 261
+      },
+      "count": 87
+    }
+
+__Get a count of the number of errors in the last 6 hours in the UAT logs__
+
+    logsearch -errors -count -from now-6h -uat | jq .
+    {
+      "_shards": {
+        "failed": 0,
+        "successful": 261,
+        "total": 261
+      },
+      "count": 419
+    }
+
+__Get first 3 errors in the last 6 hours in the logs__
+
+    logsearch -errors -from now-6h -numlogs 3 | jq .
+    {
+      "took": 94,
+      "timed_out": false,
+      "_shards": {
+        "total": 266,
+        "successful": 261,
+        "failed": 0
+      },
+      "hits": {
+        "total": 30,
+        "max_score": null,
+        "hits": [
+          {
+           "_index": "logstash-2016.02.18",
+           "_type": "logstash_tcp",
+           "_id": "AVL1dQKxm3bUyDWoQRTM",
+           "_score": null,
+           "_source": {
+             "@timestamp": "2016-02-18T16:19:44.734Z",
+             "@version": 1,
+             "message": "Failed delivery for (MessageId: ID-api-esb-prod-03-ist-berkeley-edu-47910-1455806566968-18-48 on ExchangeId: ID-api-esb-prod-03-ist-berkeley-edu-47910-1455806566968-18-46). Exhausted after delivery attempt: 1 caught: java.lang.Exception: Fetching array element 1: index is not in range 1 to 0. (180,252) UC_PKG.UC_CC_Handler.UC_C
+      [ truncated for brevity ]
+
+__Get a count of log entries where "Broker" appears in the message field in the last 24h:__
+
+Note that we can explicitly pass query parameters to the elasticsearch backend by simply making them non-flag arguments to the program.
+
+    logsearch -from now-24h -count message:Broker | jq .
+    {
+      "count": 44,
+      "_shards": {
+        "total": 266,
+        "successful": 261,
+        "failed": 0
+      }
+    }
+
+__Get a count of log entries where "Broker" appears in the message field in the last 24h from the host api-esb-prod-03. Note the use of backslashes to escape \" when passed as an explicit search term (needed for the shell):__
+
+We can add multiple search terms, but they require an explicit AND/OR between them, and they need to be capitalized. The search interface also tokenizes at non-alphanumeric characters, so the - in api-esb-prod-03 needs to be quoted so that the final search term is HOSTNAME:\"api-esb-prod-03\". This requires double escaping to get past the shell.
+
+    logsearch -from now-24h -count message:Broker AND HOSTNAME:\\\"api-esb-prod-03\\\" | jq .
+    {
+      "count": 32,
+      "_shards": {
+        "total": 266,
+        "successful": 261,
+        "failed": 0
+      }
+    }
+
+## Motivation
+
+Commandline client to query the ElasticSearch backend, bypassing Kibana
+
---
+++ logsearch.go
@@ -0,0 +1,266 @@
+package main
+//
+// Client that performs queries against ElasticSearch backend, searching among
+// records inserted using the Logstash-Logback encoder
+// Steve Chan 2/16/2016
+// sychan@berkeley.edu
+//
+// Copyright 2016, Regents of the University of California
+// Licensed under the Educational Community License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+//	You may obtain a copy of the License at: http://opensource.org/licenses/ECL-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an "AS IS"
+// BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"bytes"
+	"crypto/tls"
+	"flag"
+	"text/template"
+	"os"
+	"os/user"
+	"syscall"
+	"regexp"
+	"github.com/BurntSushi/toml"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+// Struct used for storing query configuration, as well as populating
+// the template that follows
+type Config struct {
+	UrlBase       string   // endpoint for the ElasticSearch REST service
+	Username      string   // if set, will enable basic auth authentication to REST
+	Password      string   // password to be used with basic auth
+	NumLogs       int      // number of log entries to return in query
+	FromTime      string   // start time in @timestamp for search window
+	UntilTime     string   // ending time in @timestamp for search
+	Offset        int      // used with NumLogs for offset when paginating through results
+	Count         bool     // perform only a count, do not return all matches
+	Uat           bool     // search only the UAT logs
+	Errors        bool     // search only among logs with level set to ERROR
+	CorrelationID string   // match for the camel.correlationId
+	ContextID     string   // match for the camel.contextId
+	StackTrace    string   // match for stack_track field
+	Message       string   // match for the message field
+	ExtTerms      []string // array with extra search terms appended to end of query (include AND and OR explicitly)
+}
+
+// Go template for a query against ElasticSearch, originally based on sample query from
+// Kibana dashboard
+const reqTemplate = `
+{
+{{ if not .Count }}  "fields" : [ "_source" ],{{end}}
+  "query": {
+    "filtered": {
+      "query": {
+        "bool": {
+          "should": [
+            {
+              "query_string": {
+                "query": "type:logstash_tcp AND app_homedir:\"/home/app_smx{{ if .Uat}}_sg0{{end}}/jboss-fuse-6.1.0.redhat-379\"{{if .Errors}} AND level:\"ERROR\"{{end}}{{if .CorrelationID}} AND camel.correlationId:\"{{.CorrelationID}}\"{{end}}{{if .ContextID}} AND camel.contextId:\"{{.ContextID}}\"{{end}}{{if .StackTrace}} AND stack_trace:\"{{.StackTrace}}\"{{end}}{{if .Message}} AND message:\"{{.Message}}\"{{end}}{{if .ExtTerms}} AND{{end}}{{range .ExtTerms}} {{.}}{{end}}"
+              }
+            }
+          ]
+        }
+      },
+      "filter": {
+        "bool": {
+          "must": [
+            {
+              "range": {
+                "@timestamp": {
+                  "gte": "{{ .FromTime }}",
+                  "lte": "{{ .UntilTime}}"
+                }
+              }
+            }
+          ]
+        }
+      }
+    }
+  }{{ if not .Count }},
+  "size": {{ .NumLogs }},
+  "from": {{ .Offset }},
+  "sort": [
+    {
+      "@timestamp": {
+        "order": "desc",
+        "ignore_unmapped": true
+      }
+    },
+    {
+      "@timestamp": {
+        "order": "desc",
+        "ignore_unmapped": true
+      }
+    }
+  ]
+{{end}}
+}`
+
+// Global debug flag
+var debug = false
+
+// filename for configuration file, as well as location
+// of the default configuration file
+var configFile string
+var configFileDef string
+
+var conf Config
+
+// Initialize the Config struct and commandline parsing flags as well as load in basic
+// ~/.logsearch_profile configuration
+func init() {
+	// Setup some basic configs
+	conf.UrlBase = "http://localhost:9200""
+	conf.NumLogs = 100
+	conf.FromTime = "now-30m"
+	conf.UntilTime = "now"
+	
+	usr, err := user.Current()
+	if err != nil {
+		panic( err)
+	}
+	configFileDef = usr.HomeDir + "/.logsearch_profile"
+
+	// Try to read the default config file, ignore a file not found
+	// error
+	conf2, err := ReadConfig( conf, configFileDef)
+	if err == nil {
+		conf = conf2
+	} else if os.IsExist(err) {
+		fmt.Fprintf(os.Stderr, "Failed to read file: %s\n",configFileDef)
+		panic(err)
+	}
+
+	// Setup the commandline args parser
+	flag.IntVar(&conf.NumLogs, "numlogs", conf.NumLogs, "Number of lines of matching logs to return at a time")
+	flag.IntVar(&conf.Offset, "offset", 0, "Offset into total matching logs to start")
+	flag.BoolVar(&conf.Count, "count", false, "Return only a count of the number of matches")
+	flag.BoolVar(&conf.Errors, "errors", false, "Return only error logs that match")
+	flag.BoolVar(&debug, "debug", false, "Output debug information")
+	flag.BoolVar(&conf.Uat, "uat", false, "Search among the UAT logs for matches")
+	flag.StringVar(&conf.FromTime, "from", conf.FromTime, "Time specification for starting time of the search\n\thttps://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-date-format.html\n\t")
+	flag.StringVar(&conf.UntilTime, "until", conf.UntilTime, "Time specification for ending time of search")
+	flag.StringVar(&conf.CorrelationID, "correlation", "", "camel.correlationID to match")
+	flag.StringVar(&conf.ContextID, "context", "", "camel.contextId to match")
+	flag.StringVar(&conf.StackTrace, "stack", "", "stack_trace to match")
+	flag.StringVar(&conf.Message, "message", "", "Match against the main log message")
+	flag.StringVar(&conf.Username, "username", conf.Username, "Enable basic auth by setting username")
+	flag.StringVar(&conf.Password, "password", conf.Password, "Password for basic auth")
+	flag.StringVar(&configFile,"config", configFileDef, "Location of TOML formatted configuration file https://github.com/toml-lang/toml\n\tNOTE: setting non-default configfile override comflags\n\t")
+
+}
+
+// Read the configuration file, and return a config struct, using an
+// input struct for the default values for each field. Pass any errors
+// back up in the second, error argument
+func ReadConfig(conf Config, filename string) (Config, error) {
+	_, err := toml.DecodeFile( filename, &conf)
+	if err != nil {
+		return conf, err
+	}
+	return conf, nil
+}
+
+// Perform the query and return a http.Response object
+func DoQuery( conf Config ) (*http.Response, error) {
+	url := conf.UrlBase
+	if conf.Count {
+		url += "_count"
+	} else {
+		url += "_search"
+	}
+	t, err := template.New("esquery").Parse(reqTemplate)
+	if err != nil {
+		panic(err)
+	}
+	var req_body bytes.Buffer
+	if debug {
+		err = t.Execute( &req_body, conf)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(os.Stderr, "Configuration: %+v\n",conf)
+		fmt.Fprintf(os.Stderr, "URL for query: %s\n",url)
+		fmt.Fprintf(os.Stderr, "Query request body:\n%s\n",req_body.String())
+	}
+
+	err = t.Execute( &req_body, conf)
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("GET", url, &req_body)
+	if len(conf.Username) > 0 {
+		req.SetBasicAuth( conf.Username, conf.Password)
+	}
+	
+	trans := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: trans}
+	return(client.Do(req))
+}
+
+func main() {
+	flag.Parse()
+
+	if configFile != configFileDef {
+		if debug {
+			fmt.Fprintf(os.Stderr, "Configuration file: %s\n",configFile)
+		}
+		conf2, err := ReadConfig( conf, configFile)
+		if err == nil {
+			conf = conf2
+		} else {
+			fmt.Fprintf(os.Stderr, "Failed to read file: %s\n",configFile)
+			panic(err)
+		}
+	}
+
+	// Shortcut to assign the first position non-flag argument to the message match
+	args := flag.Args()
+	if len(args) > 0 {
+		searchFilter, err := regexp.Compile("^[A-Za-z0-9_@][A-Za-z0-9_.]*:")
+		if err != nil {
+			panic( err)
+		}
+		if len(args) == 1 && ! searchFilter.MatchString(args[0]) {
+			conf.Message = args[0]
+		} else {
+			conf.ExtTerms = args
+		}
+	}
+
+	// If we have a username but no password, try prompting for it
+	if len(conf.Username) > 0 && len(conf.Password) == 0 {
+		fmt.Fprintf(os.Stderr, "Please enter password for user %s: ",conf.Username)
+		bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			panic(err)
+		}
+		conf.Password = string(bytePassword)
+		fmt.Fprintf(os.Stderr, "\n")
+
+	}
+		
+	resp, err := DoQuery(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	// fmt.Println( "response Status :", resp.Status)
+	// fmt.Println( "response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll( resp.Body)
+	fmt.Println(string(body))
+}
